671e1cb17230add799e2da00e28f8c3bd31c3940
TheresaQWQ <yxiao2044@gmail.com>
2021-02-15 15:36:54 +0800

    fix bug

---
+++ tools/after-build.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdirSync(path.join(__dirname, '../function')).forEach(e => {
+  fs.copyFileSync(path.join(__dirname, '../function', e, 'package.json'), path.join(__dirname, '../dist/function', e, 'package.json'))
+})
