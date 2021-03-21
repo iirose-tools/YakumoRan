489d668aa0f11a3a0af0170d3883d71048af8ef3
BST_luo <36332288+BSTluo@users.noreply.github.com>
2021-03-21 22:17:24 +0800

    Update after-build.js

--- tools/after-build.js
+++ tools/after-build.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+fs.mkdirSync(path.join(__dirname, '../dist/function/core'));
 
 fs.readdirSync(path.join(__dirname, '../function')).forEach(e => {
   try {
     fs.mkdirSync(path.join(__dirname, '../function', e));
   } catch (error) {}
-  fs.copyFileSync(path.join(__dirname, '../function', e, 'package.json'), path.join(__dirname, '../dist/function', e, 'package.json'))
+  fs.copyFileSync(path.join(__dirname, '../function', e, 'package.json'), path.join(__dirname, '../dist/function', e, 'package.json'));
 })
+
