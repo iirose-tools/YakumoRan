137932da1abef22006bae0472ae9966beeb4b0ce
BST_luo <36332288+BSTluo@users.noreply.github.com>
2021-03-23 07:58:09 +0800

    Add files via upload

--- tools/after-build.js
+++ tools/after-build.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const fs = require('fs');
+if (!fs.existsSync(path.join(__dirname, '../dist/function/core'))) {
+  fs.mkdirSync(path.join(__dirname, '../dist/function/core'));
+}
+
 
 fs.readdirSync(path.join(__dirname, '../function')).forEach(e => {
   try {
     fs.mkdirSync(path.join(__dirname, '../function', e));
   } catch (error) {}
-  fs.copyFileSync(path.join(__dirname, '../function', e, 'package.json'), path.join(__dirname, '../dist/function', e, 'package.json'))
+  fs.copyFileSync(path.join(__dirname, '../function', e, 'package.json'), path.join(__dirname, '../dist/function', e, 'package.json'));
 })
