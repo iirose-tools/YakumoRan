9a30edff14b81843181f183610118716b48a55d4
BST_luo <36332288+BSTluo@users.noreply.github.com>
2021-03-28 20:49:40 +0800

    概率游戏
    优化结构

--- function/probability/index.ts
+++ function/probability/index.ts
@@ -12,19 +12,19 @@ try {
 const random = (n: number, m: number): number => { return Math.floor(Math.random() * (m - n + 1) + n) }
 
 // 获取玩家的金币
-const getMoney = (username: string) => {
-  const wordPath = path.join(__dirname, `../../data/probability/${username}.json`)
+const getMoney = () => {
+  const wordPath = path.join(__dirname, '../../data/probability/probability.json')
   if (!fs.existsSync(wordPath)) {
-    fs.writeFileSync(wordPath, '{"nowMoney": 350}')
+    fs.writeFileSync(wordPath, '{}')
   }
 
   return JSON.parse(fs.readFileSync(wordPath).toString())
 }
 
 // 更新json文件
-const update = (file: any, tyf: string) => {
+const update = (file: any) => {
   try {
-    fs.writeFileSync(path.join(__dirname, `../../data/probability/${tyf}.json`), JSON.stringify(file, null, 3))
+    fs.writeFileSync(path.join(__dirname, '../../data/probability/probability.json'), JSON.stringify(file, null, 3))
     logger('Word').info('文件写入成功')
   } catch (error) {
     logger('Word').warn('文件写入失败', error)
@@ -39,28 +39,32 @@ const fitter = (txt: string) => {
 
 // 核心源码
 api.command(/^\.雨铭压(.*)$/, async (m, e, reply) => {
-  const nowMoney = getMoney(e.username)
+  const nowMoney = getMoney()
   const m1 = Number(m[1])
   if (isNaN(m1)) {
     reply('你输入的似乎不是数字哦~换成数字再试一下吧', config.app.color)
-  } else if (nowMoney.nowMoney >= m1) {
+  }
+  if (nowMoney[e.username] == null) {
+    nowMoney[e.username] = 350
+  }
+  if (nowMoney[e.username] >= m1) {
     if (random(0, 1) === 0) {
-      nowMoney.nowMoney = nowMoney.nowMoney - m1
-      update(nowMoney, e.username)
-      reply(` [*${e.username}*]   :  余额 - ${m1} 钞   ❌   ,   💰 ${String(nowMoney.nowMoney)} 钞`, config.app.color)
+      nowMoney[e.username] = nowMoney[e.username] - m1
+      update(nowMoney)
+      reply(` [*${e.username}*]   :  余额 - ${m1} 钞   ❌   ,   💰 ${String(nowMoney[e.username])} 钞`, config.app.color)
     } else {
-      nowMoney.nowMoney = nowMoney.nowMoney + m1
-      update(nowMoney, e.username)
-      reply(` [*${e.username}*]   :  余额 + ${m1} 钞   ✔️   ,   💰 ${String(nowMoney.nowMoney)} 钞`, config.app.color)
+      nowMoney[e.username] = nowMoney[e.username] + m1
+      update(nowMoney)
+      reply(` [*${e.username}*]   :  余额 + ${m1} 钞   ✔️   ,   💰 ${String(nowMoney[e.username])} 钞`, config.app.color)
     }
   } else {
-    reply(` [*${e.username}*]   :  抱歉  ,  您的余额不足  ,  您的当前余额为  :  ${String(nowMoney.nowMoney)} 钞`, config.app.color)
+    reply(` [*${e.username}*]   :  抱歉  ,  您的余额不足  ,  您的当前余额为  :  ${String(nowMoney[e.username])} 钞`, config.app.color)
   }
 })
 
 // 查看钱包
 api.command(/^\.查看钱包(.*)$/, async (m, e, reply) => {
   const name: string = fitter(m[1])
-  const nowMoney = getMoney(name)
-  reply(` [*${e.username}*]   :  【${name}】 的余额为  :  ${String(nowMoney.nowMoney)}钞`, config.app.color)
+  const nowMoney = getMoney()
+  reply(` [*${e.username}*]   :  【${name}】 的余额为  :  ${String(nowMoney[name])}钞`, config.app.color)
 })
