439f63df1348bb5364e2a6bbb8dcfae31d8e40ea
BST_luo <36332288+BSTluo@users.noreply.github.com>
2021-04-12 22:07:56 +0800

    Update index.ts

--- function/tools/index.ts
+++ function/tools/index.ts
@@ -4,9 +4,3 @@ import * as api from '../../lib/api'
 api.command(/^赞我$/, (m, e, reply) => {
   api.method.like(e.uid, 'qwq')
 })
-
-api.command(/^飞到(.*)$/, (m, e, reply) => {
-  if (e.uid !== config.app.master_uid) return
-  const m1 = m[1].replace(/[\s[\]_]/g, '')
-  api.method.bot.moveTo(m1)
-})
