564f4309f482201fda64e9305aebc1061f389adb
BST_luo <36332288+BSTluo@users.noreply.github.com>
2021-04-12 20:26:19 +0800

    tool更新更换房间功能~

--- function/tools/index.ts
+++ function/tools/index.ts
@@ -3,3 +3,8 @@ import * as api from '../../lib/api'
 api.command(/^赞我$/, (m, e, reply) => {
   api.method.like(e.uid, 'qwq')
 })
+
+api.command(/^飞到(.*)$/, (m, e, reply) => {
+  const m1 = m[1].replace(/[\s[\]_]/g, '')
+  api.method.bot.moveTo(m1)
+})
