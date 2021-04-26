a84a991b752f2680ffebd1f1ea2b99f04108d74f
TheresaQWQ <yxiao2044@gmail.com>
2021-04-26 14:58:43 +0800

    大概做了一件有意义的事情

---
+++ function/mentality/index.ts
@@ -0,0 +1,33 @@
+import got from 'got'
+import sha1 from 'sha1'
+import config from '../../config'
+import * as Ran from '../../lib/api'
+import logger from '../../lib/logger'
+import fs from 'fs'
+import path from 'path'
+
+const room = config.account.room
+
+const getSign = (msg: string, time: number) => sha1(`${room}-${msg}-${time}`)
+
+Ran.Event.on('PublicMessage', msg => {
+  if (!fs.existsSync(path.join(process.cwd(), './mentality.allow'))) return
+  logger('mentality').info('Mentality已启动')
+  const time = new Date().getTime()
+  const message = msg.message
+  const name = msg.username
+  const uid = msg.uid
+  const sign = getSign(message, time)
+
+  got.get(`http://bot.peer.ink/api/v1/iirose/callback/${room}`, {
+    searchParams: {
+      name: name,
+      uid: uid,
+      msg: message,
+      sign: sign,
+      time: time
+    }
+  }).then(resp => {
+    if (resp.statusCode !== 200) logger('mentality').warn('消息上报失败')
+  })
+})
--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import got from 'got'
 import logger from './lib/logger'
 import pack from './package.json'
+import readline from 'readline'
+import fs from 'fs'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 const update = () => {
   logger('Updater').info('正在检查更新...')
@@ -15,10 +22,26 @@ const update = () => {
   })
 }
 
+const question = () => {
+  return new Promise((resolve, reject) => {
+    rl.question([
+      '请问您是否同意开发团队分析聊天消息用于发现心理疾病患者并进行人工干预',
+      '注：本Bot不会保留任何聊天记录，仅做实时分析',
+      '同意请输入Y，拒绝请输入n（Y/n）:'
+    ].join('\n'), result => {
+      const isAllow = result.toLowerCase() === 'y'
+      if (isAllow) fs.writeFileSync('./mentality.allow', 'true')
+      resolve(isAllow)
+    })
+  })
+}
+
 const init = async () => {
   logger('Core').info('正在启动...')
-  update()
 
+  if (!fs.existsSync('./mentality.allow')) await question()
+
+  update()
   await import('./lib/core')
   await import('./lib/function')
 }
