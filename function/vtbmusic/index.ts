c57cb5c00e71cfefd951894910447f7f23d37002
TheresaQWQ <yxiao2044@gmail.com>
2021-05-06 16:39:53 +0800

    优化

--- function/vtbmusic/index.ts
+++ function/vtbmusic/index.ts
@@ -1,9 +1,10 @@
 import config from '../../config'
 import * as api from '../../lib/api'
 import { vtbmusic } from './api'
+import permission from '../permission/permission'
 
-api.command(/^vtb点歌 (.*)$/, (m, e, reply) => {
-  vtbmusic.search(m[1]).then(e => {
+api.command(/^vtb点歌(.*)$/, (m, e, reply) => {
+  vtbmusic.search(m[1].trim()).then(e => {
     if (e) {
       api.method.sendMedia('music', e.title, e.signer, e.cover, `https://vtbmusic.com/song?id=${e.id}`, e.music, e.duration, e.bitrate, config.app.color)
     } else {
@@ -20,4 +21,10 @@ api.command(/^vtb点歌$/, (m, e, reply) => {
       reply('[vtbmusic] 点歌失败', config.app.color)
     }
   })
+})
+
+api.command(/^vcut$/, (m, e, reply) => {
+  if (permission.users.hasPermission(e.uid, 'vtbmusic.cut')) {
+    reply('cut', config.app.color)
+  }
 })
