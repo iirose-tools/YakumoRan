9b6bbde91901f89cb67be6e082d51ae23bc7616c
TheresaQWQ <yxiao2044@gmail.com>
2021-05-06 18:39:17 +0800

    单元测试！

--- test/vtbmusic.test.ts
+++ test/vtbmusic.test.ts
@@ -2,7 +2,6 @@
 import '../index'
 import permission from '../function/permission/permission'
 import { commands } from '../lib/api'
-import { onSend } from '../lib/utils'
 
 test('vtbmusic.cut', async () => {
   await new Promise<void>((resolve, reject) => {
@@ -15,31 +14,3 @@ test('vtbmusic.cut', async () => {
     })
   })
 }, 5000)
-
-test('vtbmusic.play.default', async () => {
-  await new Promise<void>((resolve, reject) => {
-    // @ts-ignore
-    commands['vtbmusic.play.default']([], { uid: 'user' }, (msg, color) => {
-      expect(1).toBe(0)
-      resolve()
-    })
-    onSend(/m__4=/, () => {
-      expect(1).toBe(1)
-      resolve()
-    })
-  })
-}, 1e4)
-
-test('vtbmusic.play', async () => {
-  await new Promise<void>((resolve, reject) => {
-    // @ts-ignore
-    commands['vtbmusic.play'](['', 'KING'], { uid: 'user' }, (msg, color) => {
-      expect(1).toBe(0)
-      resolve()
-    })
-    onSend(/m__4=/, () => {
-      expect(1).toBe(1)
-      resolve()
-    })
-  })
-}, 1e4)
