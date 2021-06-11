752a7e202625fb83e7ed4c2a06b64a10de14bea7
TheresaQWQ <yxiao2044@gmail.com>
2021-06-11 20:20:15 +0800

    fix bug

--- test/whois.test.ts
+++ test/whois.test.ts
@@ -2,30 +2,10 @@
 import '../index'
 import { commands } from '../lib/api'
 
-test('whois.ip', async () => {
+test('whois.do', async () => {
   await new Promise<void>((resolve, reject) => {
     // @ts-ignore
-    commands['whois.ip'](['', '1.1.1.1'], {}, (msg, color) => {
-      expect(msg).toMatch(/https:\/\/pastebin.ubuntu.com/)
-      resolve()
-    })
-  })
-}, 5000)
-
-test('whois.asn', async () => {
-  await new Promise<void>((resolve, reject) => {
-    // @ts-ignore
-    commands['whois.asn'](['', '4314'], {}, (msg, color) => {
-      expect(msg).toMatch(/https:\/\/pastebin.ubuntu.com/)
-      resolve()
-    })
-  })
-}, 5000)
-
-test('whois.domain', async () => {
-  await new Promise<void>((resolve, reject) => {
-    // @ts-ignore
-    commands['whois.domain'](['', 'github.com'], {}, (msg, color) => {
+    commands['whois.do'](['', 'github.com'], {}, (msg, color) => {
       expect(msg).toMatch(/https:\/\/pastebin.ubuntu.com/)
       resolve()
     })
