554ae08ef3d1bfbd339441e8713595b52530d3ce
TheresaQWQ <yxiao2044@gmail.com>
2021-06-21 07:39:01 +0800

    修复模拟赌博会触发聊天插件的bug

--- function/chat/index.ts
+++ function/chat/index.ts
@@ -6,6 +6,7 @@ import util from 'util'
 if (!config.function.chat.disable) {
   api.Event.on('PublicMessage', async (msg) => {
     if (msg.username === config.account.username) return // 不响应自己发送的消息
+    if (new RegExp(`^${config.app.nickname}压(.*)$`).test(msg.message)) return // 不响应模拟赌博消息
 
     const reply = (message: string) => {
       const data = `${msg.message} (_hr) ${msg.username}_${Math.round(new Date().getTime() / 1e3)} (hr_) ${message}`
