b1681587ab252fa00918434ecb1e1a7bc75a7585
BST_luo <36332288+BSTluo@users.noreply.github.com>
2022-03-26 21:00:26 +0800

    修复了一些bug以及更新了一些功能

--- index.ts
+++ index.ts
@@ -1,31 +1,96 @@
-import got from 'got'
-import logger from './lib/logger'
-import pack from './package.json'
+import Word from './api/main'
+import * as api from '../../lib/api'
+import config from '../../config'
+import per from '../permission/permission'
+import fs from 'fs'
+import path from 'path'
 
-const update = () => {
-  logger('Updater').info('正在检查更新...')
+try {
+  fs.mkdirSync(path.join(api.Data, 'word/wordData'))
+} catch (err) {}
 
-  got.get('https://api.peer.ink/api/github/YakumoRan').then(resp => {
-    const data = JSON.parse(resp.body)
-    if (data.version !== pack.version) {
-      logger('Updater').info(`发现了新的版本，版本号为 ${data.version}，请及时更新，链接: https://github.com/iirose-tools/YakumoRan`)
-    } else {
-      logger('Updater').info('未发现新版本')
-    }
-  })
+try {
+  fs.mkdirSync(path.join(api.Data, 'word/userData'))
+} catch (err) {}
+
+try {
+  fs.mkdirSync(path.join(api.Data, 'word/wordlist'))
+} catch (err) {}
+
+// 指定词库数据库根目录
+const word = new Word(api.Data)
+
+// 主词库√
+api.Event.on('PublicMessage', msg => {
+  if (per.users.hasPermission(msg.uid, 'word.kick')) return // 不响应已经被踢出的人
+  if (msg.username === config.account.username) return // 不响应自己发送的消息
+  const wd: string = msg.message.trim()
+  const reply = api.method.sendPublicMessage // 定义发送文本的函数
+  const out = word.start(wd, msg)
+  if (out) {
+    reply(out)
+  }
 }
+)
 
-const init = async () => {
-  logger('Core').info('正在启动...')
-
-  update()
-  await import('./lib/functions')
-  await import('./lib/core')
+// 添加问答√
+api.command(/^\.问(.*?)答(.*)$/, 'word.add', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.add') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  reply(word.add(m[1], m[2], e))
 }
+)
 
-process.on('uncaughtException', (err: any, origin: any) => {
-  logger('uncaughtException').error(err, origin)
-  process.exit(127)
-})
+// 删除问答√
+api.command(/^\.删(.*?)序号(.*)$/, 'word.del', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.del') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  reply(word.del(m[1], m[2], e))
+}
+)
 
-init()
+// 查看问（包含指定文字的问，需要显示是哪个库的√
+api.command(/^\.问表(.*?)$/, 'word.qwhere', async (m, e, reply) => {
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  api.method.sendPrivateMessage(e.uid, word.getas(m[1]))
+}
+)
+
+// 查看答 （包含指定文字的答，需要显示是哪个库的√
+api.command(/^\.答表(.*?)$/, 'word.awhere', async (m, e, reply) => {
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  api.method.sendPrivateMessage(e.uid, word.getqs(m[1]))
+}
+)
+
+// 设置为第几个词库
+api.command(/^\.入库(.*?)$/, 'word.in', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.in') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+
+  reply(word.in(m[1], e))
+}
+)
+
+api.command(/^\.出库$/, 'word.out', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.out') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  reply(word.out(e))
+}
+)
+
+api.command(/^\.表(.*)$/, 'word.list', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.out') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  reply(word.list(m[1], e))
+}
+)
+
+/**
+ * 导出，导入词库功能
+ * 以node POST给服务器然后导出为直链
+ * 导入为下载直链
+ * 每隔一天清空一次缓存
+ *
+ * 删除一句、全部删除
+*/
