40bf93e29f742090c699b1ab039b2979e4c8aae2
BST_luo <36332288+BSTluo@users.noreply.github.com>
2022-05-14 12:40:39 +0800

    update
    新增多个标签

---
+++ function/word/api/edit.ts
@@ -0,0 +1,171 @@
+import fs from 'fs'
+import path from 'path'
+
+/**
+    * 返回一个文件的json对象
+    * @param list 词库文件目录（wordconfig/userData/wordData）
+    * @param name 词库文件名
+    * @return 词库json对象
+  */
+const getjson = (list:string, name:string, dir:any) => {
+  const wordPath = path.join(dir, `./word/${list}/${name}.json`)
+  if (!fs.existsSync(wordPath)) {
+    fs.writeFileSync(wordPath, '{}')
+  }
+  return JSON.parse(fs.readFileSync(wordPath).toString())
+}
+
+/**
+    * 将词库json对象存储在文件内
+    * @param list 词库文件目录
+    * @param name 词库文件名
+    * @param file 词库json对象
+  */
+const update = (list:string, name:string, file:any, dir:any) => {
+  try {
+    fs.writeFileSync(path.join(dir, `./word/${list}/${name}.json`), JSON.stringify(file, null, 3))
+  } catch (error) {
+  }
+}
+
+export const edit = {
+  /**
+    * 添加词库
+    * @param q 指定触发词
+    * @param a 指定触发后的回复
+    * @param m 指定编辑者数据json：{uid:"id",name:"昵称"}
+    * @return 返回为字符串，成功/失败
+  */
+  add (q:string, a:string, m:any, dir:any) {
+    // 获取json对象后判断key是否存在，不存在则定义为数组，若存在则为数组添加元素，并存储它
+    const uid = m.uid
+    let listName = ''
+    const list = getjson('wordconfig', 'userlist', dir)
+    if (Object.prototype.hasOwnProperty.call(list, uid)) {
+      listName = list[uid]
+    } else {
+      listName = '默认'
+    }
+
+    const word = getjson('wordData', listName, dir)
+    if (word[q] == null) {
+      word[q] = []
+    }
+    if (word[q][a]) return (`  [ 词库核心 ]  该词条已存在于词库【${listName}】，序号为【${word[q].indexOf(a) + 1}】`)
+    const num = word[q].push(a)
+    update('wordData', listName, word, dir)
+    return (`  [ 词库核心 ]  添加成功，已添加到词库【${listName}】，序号为【${num}】`)
+  },
+
+  /**
+    * 删除词库
+    * @param q 指定触发词
+    * @param num 指定第几条回复（若为'all'则删除整个词条）
+    * @param m 指定编辑者数据json：{uid:"id",name:"昵称"}
+    * @return 返回为字符串，成功/失败
+  */
+  del (q:string, num:string, m:any, dir:any) {
+    const id = m.uid
+    // 获取json对象后，删除其中的一项，若删除后数组为空则删除key，存储数组
+    const ku = getjson('wordconfig', 'userlist', dir)
+    let word
+    let outList = ''
+    if (ku[id]) {
+      outList = ku[id]
+      word = getjson('wordData', ku[id], dir)
+    } else {
+      outList = '默认'
+      word = getjson('wordData', '默认', dir)
+    }
+    if (!word[q]) { return '  [ 词库核心 ]  并未在当前库中找到相应的关键词' }
+    if (num === 'all') {
+      delete word[q]
+    } else {
+      word[q].splice(Number(num) - 1, 1)
+      if (!word[q].length) {
+        delete word[q]
+      }
+    }
+
+    update('wordData', outList, word, dir)
+    return (`  [ 词库核心 ]  删除成功，词库【${outList}】，处理成功`)
+  },
+
+  /**
+    * 选择编辑词库
+    * @param db 设置被编辑词库
+    * @param m 设置人数据json：{uid:"id",name:"昵称"}
+    * @return 返回为回复结果（成功/失败）
+  */
+  in (db:string, m:any, dir:any) {
+    const uid = m.uid
+    const list = getjson('wordconfig', 'userlist', dir)
+    list[uid] = db
+    update('wordconfig', 'userlist', list, dir)
+    return ` [词库核心] 设置成功，接下来您添加的词库将添加至【${db}】词库`
+  },
+
+  /**
+      * 恢复编辑默认词库
+      * @param m 设置人数据json：{uid:"id",name:"昵称"}
+      * @return 返回为回复结果（成功/失败）
+    */
+  out (m:any, dir:any) {
+    const uid = m.uid
+    const list = getjson('wordconfig', 'userlist', dir)
+    try {
+      delete list[uid]
+      update('wordconfig', 'userlist', list, dir)
+      return ' [词库核心] 设置成功，将添加至【默认】词库'
+    } catch (err) { return ' [词库核心] 设置失败' }
+  },
+
+  /**
+    * 增加词库管理员
+    * @name 唯一标识（字符串）
+    * @return 返回为结果（字符串）
+  */
+  op (name:string, dir:any) {
+    let id = ''
+    const a = name.match(/\s*\[@(.*?)@\]\s*/)
+    if (a) {
+      id = a[1]
+    }
+    const adminlist = getjson('wordconfig', 'adminlist', dir)
+    if (adminlist.admin.indexOf(id) < 0) {
+      adminlist.admin.push(id)
+    }
+    update('wordconfig', 'adminlist', adminlist, dir)
+    return ' [词库核心] 词库管理员设置成功...!'
+  },
+
+  /**
+    * 去除词库管理员
+    * @name 唯一标识（字符串）
+    * @return 返回为结果（字符串）
+  */
+  deop (name:string, dir:any) {
+    let id = ''
+    const a = name.match(/\s*\[@(.*?)@\]\s*/)
+    if (a) {
+      id = a[1]
+    }
+    const adminlist = getjson('wordconfig', 'adminlist', dir)
+    if (adminlist.admin.indexOf(id) >= 0) {
+      adminlist.admin.splice(adminlist.admin.indexOf(id), 1)
+    }
+    update('wordconfig', 'adminlist', adminlist, dir)
+    return ' [词库核心] 词库管理员取消成功...!'
+  },
+
+  /**
+    * 删除某一词库
+    * @param name 需要删除的词库
+    * @return 返回结果
+  */
+  over (name:string, dir:any) {
+    fs.unlink(path.join(dir, `./word/wordData/${name}.json`), (err) => {
+      if (err) throw err
+    })
+  }
+}
--- function/word/index.ts
+++ function/word/index.ts
@@ -1,10 +1,10 @@
-import Word from './api/main'
+import Word from './api/'
 import * as api from '../../lib/api'
 import config from '../../config'
 import per from '../permission/permission'
 import fs from 'fs'
 import path from 'path'
-import axios from 'axios'
+import got from 'got'
 
 try {
   fs.mkdirSync(path.join(api.Data, 'word/wordData'))
@@ -21,15 +21,48 @@ try {
 // 指定词库数据库根目录
 const word = new Word(api.Data, config.app.master_uid, config.app.nickname)
 
+// 输出公开/私聊的数组
+const outputResolution = (wd:string) => {
+  const publicValue:any = []
+  const privateValue:any = []
+  if (!wd) return null
+
+  while (wd.match(/\$@(.*?)@\$/)) {
+    if (wd.match(/\$@(.*?)@\$/)) {
+      const text:any = wd.match(/\$@(.*?)@\$/)
+      if (text) {
+        publicValue.push(text[1])
+        wd = wd.replace(text[0], '')
+      }
+    }
+  }
+
+  while (wd.match(/\$\/(.*?)\/\$/)) {
+    if (wd.match(/\$\/(.*?)\/\$/)) {
+      const text:any = wd.match(/\$\/(.*?)\/\$/)
+      if (text) {
+        privateValue.push(text[1])
+        wd = wd.replace(text[0], '')
+      }
+    }
+  }
+
+  return [publicValue, privateValue, wd]
+}
+
 // 主词库(公屏触发)
 api.Event.on('PrivateMessage', msg => {
   if (per.users.hasPermission(msg.uid, 'word.kick')) return // 不响应已经被踢出的人
   if (msg.username === config.account.username) return // 不响应自己发送的消息
   const wd: string = msg.message.trim()
-  const reply = api.method.sendPrivateMessage // 定义发送文本的函数
-  const out = word.start(wd, msg)
-  if (out) {
-    reply(msg.uid, out)
+  const out:any = word.start(wd, msg)
+
+  const outputText = outputResolution(out)
+
+  if (outputText) {
+    if (outputText[2].length) { api.method.sendPublicMessage(outputText[2]) }
+    if (outputText[1].length) { api.method.sendPrivateMessage(msg.uid, outputText[1].join('')) }
+    if (outputText[0].length) { api.method.sendPublicMessage(outputText[0].join('')) }
   }
 }
 )
@@ -39,10 +72,14 @@ api.Event.on('PublicMessage', msg => {
   if (per.users.hasPermission(msg.uid, 'word.kick')) return // 不响应已经被踢出的人
   if (msg.username === config.account.username) return // 不响应自己发送的消息
   const wd: string = msg.message.trim()
-  const reply = api.method.sendPublicMessage // 定义发送文本的函数
-  const out = word.start(wd, msg)
-  if (out) {
-    reply(out)
+  const out:any = word.start(wd, msg)
+
+  const outputText = outputResolution(out)
+
+  if (outputText) {
+    if (outputText[2].length) { api.method.sendPublicMessage(outputText[2]) }
+    if (outputText[1].length) { api.method.sendPrivateMessage(msg.uid, outputText[1].join('')) }
+    if (outputText[0].length) { api.method.sendPublicMessage(outputText[0].join('')) }
   }
 }
 )
@@ -79,7 +116,7 @@ api.command(/^\.答表(.*?)$/, 'word.awhere', async (m, e, reply) => {
 
 // 设置为第几个词库
 api.command(/^\.入库(.*?)$/, 'word.in', async (m, e, reply) => {
-  if (!per.users.hasPermission(e.uid, 'word.edit.in') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (!per.users.hasPermission(e.uid, `word.in.${m[1]}`) && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
   if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
 
   reply(word.in(m[1], e))
@@ -97,6 +134,13 @@ api.command(/^\.表(.*)$/, 'word.list.a', async (m, e, reply) => {
   if (!per.users.hasPermission(e.uid, 'word.edit.find') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
   if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
   api.method.sendPrivateMessage(e.uid, word.alist(m[1], e))
+}
+)
+
+api.command(/^\.库表(.*)$/, 'word.list.find', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.find') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  api.method.sendPrivateMessage(e.uid, word.findList(m[1]))
 }
 )
 
@@ -140,8 +184,11 @@ api.command(/^.上传(.*)$/, 'word.upload', async (m, e, reply) => {
   const up = word.getjson('wordData', m[1])
   if (JSON.stringify(up) !== '{}') {
     try {
-      const response = await axios.post('https://word.bstluo.top/new.php', up)
-      reply(` [词库核心] ${response.data}`)
+      const response = await got('https://word.bstluo.top/new.php', {
+        method: 'post',
+        json: up
+      })
+      reply(` [词库核心] ${response.body}`)
     } catch (error) {
       reply('投稿失败，请联系管理员手动进行投稿')
     }
@@ -152,21 +199,54 @@ api.command(/^.下载(.*):(.*)$/, 'word.download', async (m, e, reply) => {
   if (!per.users.hasPermission(e.uid, 'word.edit.download') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
   if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
   try {
-    const response = await axios.post('https://word.bstluo.top/read.php', {
-      id: m[1]
+    const response = await got('https://word.bstluo.top/read.php', {
+      method: 'post',
+      json: {
+        id: m[1]
+      }
     })
-    word.update('wordData', m[2], response.data)
+    word.update('wordData', m[2], JSON.parse(response.body.toString()))
     reply(' [词库核心] 下载成功')
   } catch (error) {
     console.log(error)
     reply('下载失败，请联系管理员手动进行投稿')
   }
 })
-/**
- * 导出，导入词库功能
- * 以node POST给服务器然后导出为直链
- * 导入为下载直链
- * 每隔一天清空一次缓存
- *
- * 删除一句、全部删除
-*/
+
+api.command(/^\.删库(.*)$/, 'word.edit.over', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.over') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  reply(word.over(m[1]))
+}
+)
+
+api.command(/^.增量(.*):(.*)$/, 'word.download.add', async (m, e, reply) => {
+  if (!per.users.hasPermission(e.uid, 'word.edit.download') && !per.users.hasPermission(e.uid, 'permission.word')) return // 不响应没有权限的人，@后期改为能设置config文件内决定是否开启这一条
+  if (per.users.hasPermission(e.uid, 'word.kick')) return // 不响应已经被踢出的人
+  try {
+    const response = await got('https://word.bstluo.top/read.php', {
+      method: 'post',
+      json: {
+        id: m[1]
+      }
+    })
+    word.incrementalUpdat('wordData', m[2], JSON.parse(response.body.toString()))
+    reply(' [词库核心] 下载成功')
+  } catch (error) {
+    console.log(error)
+    reply('下载失败，请联系管理员手动进行投稿')
+  }
+})
+
+// 更新格式转换(?啥来着)
+// *添加词库时显示哪些库也含有此选项
+
+// 已完成
+// 入库需要word.in.库名才可入库
+// 查库可以放相关库名.库表x
+// 新增技能和道具槽
+// 修复了一个bug
+// 禁解析符号$[语句]$
+// 私聊标签$@语句@$
+// 公聊标签$/语句/$
+// 增量指令(下载词库的增量)
