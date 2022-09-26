b8db09e2abd6d3908fae0ee3486095ba3a6a7fd3
TheresaQWQ <yxiao2044@gmail.com>
2022-09-26 18:18:20 +0800

    fix: commander貌似没法用? 去掉了commander自己写了个简单的命令行工具

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
-const { App } = require('@yakumoran/core')
+const { App } = require('../dist')
 const { promises: fs, existsSync } = require('fs')
 const { join } = require('path')
-const { createCommand } = require('commander')
 const { promises: readline } = require('readline')
 const { exec } = require('child_process')
+const crypto = require('crypto')
 
 const question = async (query) => {
   const rl = readline.createInterface({
@@ -17,76 +17,86 @@ const question = async (query) => {
   return answer
 }
 
+const md5 = (str) => crypto.createHash('md5').update(str).digest('hex')
+
+const init = async () => {
+  const username = await question('机器人用户名: ')
+  const password = md5(await question('机器人密码: '))
+  const color = await question('消息气泡颜色(不含#): ')
+  const masterUid = await question('主人UID: ')
+  const masterName = await question('主人用户名: ')
+  const port = await question('网页面板端口: ')
+  const room = await question('房间ID: ')
+  const roomPassword = await question('房间密码(无密码请留空): ')
+
+  const config = {
+    bot: {
+      username,
+      password,
+      master_uid: masterUid,
+      master_name: masterName,
+      color,
+      room,
+      room_password: roomPassword,
+      port
+    },
+    plugins: {},
+    database: {
+      client: 'better-sqlite3',
+      connection: {
+        filename: `${join(process.cwd(), 'data', 'database.sqlite')}`
+      }
+    }
+  }
+
+  await fs.writeFile(join(process.cwd(), 'config.json'), JSON.stringify(config, null, 2))
+  await fs.mkdir(join(process.cwd(), 'data'))
+
+  console.log('配置文件创建完成，默认使用better-sqlite3数据库，如需使用其他数据库请手动修改配置文件')
+  const install = await question('要安装依赖吗? (y/n): ')
+  if (install === 'y') {
+    console.log('正在安装依赖...')
+    const p = exec('npm install')
+    p.on('exit', () => {
+      const p = exec('npm install better-sqlite3')
+      p.on('exit', () => console.log('安装完成'))
+    })
+  } else {
+    console.log('配置完成，您可能需要手动安装依赖')
+  }
+
+  console.log('输入 yakumoran-cli run 运行机器人')
+}
+
+const run = async () => {
+  const confPath = join(process.cwd(), 'config.json')
+
+  if (!existsSync(confPath)) {
+    console.log('配置文件不存在，请先运行init命令初始化配置文件')
+    return
+  }
+
+  const config = JSON.parse(await fs.readFile(confPath, 'utf-8'))
+  const app = new App(config)
+
+  for (const plugin of Object.keys(config.plugins)) {
+    app.loadPlugin(plugin)
+  }
+}
+
 const main = async () => {
-  createCommand('init')
-    .description('初始化一个新的机器人')
-    .action(async () => {
-      const username = await question('机器人用户名: ')
-      const password = await question('机器人密码: ')
-      const color = await question('消息气泡颜色(不含#): ')
-      const masterUid = await question('主人UID: ')
-      const masterName = await question('主人用户名: ')
-      const port = await question('网页面板端口: ')
-      const room = await question('房间ID: ')
-      const roomPassword = await question('房间密码(无密码请留空): ')
+  const subcommand = process.argv[2]
 
-      const config = {
-        bot: {
-          username,
-          password,
-          master_uid: masterUid,
-          master_name: masterName,
-          color,
-          room,
-          room_password: roomPassword,
-          port
-        },
-        plugins: {},
-        database: {
-          client: 'better-sqlite3',
-          connection: {
-            filename: `${join(process.cwd(), 'data', 'database.sqlite')}`
-          }
-        }
-      }
+  if (subcommand === 'init') return init()
+  if (subcommand === 'run') return run()
 
-      await fs.writeFile(join(process.cwd(), 'config.json'), JSON.stringify(config, null, 2))
-
-      console.log('配置文件创建完成，默认使用better-sqlite3数据库，如需使用其他数据库请手动修改配置文件')
-      const install = await question('要安装依赖吗? (y/n): ')
-      if (install === 'y') {
-        const p = exec('npm install')
-        p.stderr?.pipe(process.stderr)
-        p.stdout?.pipe(process.stdout)
-
-        p.on('exit', () => {
-          const p = exec('npm install better-sqlite3')
-
-          p.stderr?.pipe(process.stderr)
-          p.stdout?.pipe(process.stdout)
-
-          p.on('exit', () => console.log('安装完成'))
-        })
-      }
-    })
-
-  createCommand('run')
-    .description('运行机器人')
-    .action(async () => {
-      const confPath = join(process.cwd(), 'config.json')
-
-      if (!existsSync(confPath)) {
-        console.log('配置文件不存在，请先运行init命令初始化配置文件')
-        return
-      }
-
-      const config = JSON.parse(await fs.readFile(confPath, 'utf-8'))
-      const app = new App(config)
-
-      for (const plugin of Object.keys(config.plugins)) {
-        app.loadPlugin(plugin)
-      }
-    })
+  console.log([
+    'Usage: yakumoran-cli <command>',
+    '',
+    'Commands:',
+    '  init  初始化新的机器人',
+    '  run   运行机器人'
+  ].join('\n'))
 }
 
 main()
