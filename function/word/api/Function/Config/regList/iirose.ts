05325a555300ede5dc9c810a09be29111a830076
BST_luo <36332288+BSTluo@users.noreply.github.com>
2024-02-09 21:30:26 +0800

    Delete function/word directory

--- function/word/api/Function/Config/regList/iirose.ts
+++
@@ -1,6 +0,0 @@
-export const regList: [RegExp, string, string][] = [
-  [/\s*\[\*([\s\S]+)\*\]\s*/, '(@)', 'yname'], // 对方昵称
-  [/\s*\[@([\s\S]+)@\]\s*/, '(id)', 'yid'], // 对方id
-  [/<([\s\S]+)>/, '<语>', 'text'],
-  [/\s*(\d+)\s*/, '(数)', 'number']
-]
